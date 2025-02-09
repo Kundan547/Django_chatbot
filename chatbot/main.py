88a32446ebe0d0f0f726cc1a0c9493cd60acb5b4
Kundan547 <kundanvyas197@gmail.com>
2025-02-09 12:22:43 +0530

    Added a new function to get the answers for questions.

--- chatbot/main.py
+++ chatbot/main.py
@@ -19,3 +19,9 @@ def save_knowledge_base(file_path: str, data:dict):
 def find_best_match(user_question: str, questions: list[str]) -> str | None:
     matches: list = get_close_matches(user_question, questions, n=1, cutoff=0.8)
     return matches[0] if matches else None
+
+#Get answers for questions.
+def get_answer_for_question(question: str, knowledge_base: dict) -> str | None:
+    for q in knowledge_base["questions"]:
+        if q["question"] == question:
+            return q['answer']
